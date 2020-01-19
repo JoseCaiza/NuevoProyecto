f1d5336f09fde893a9ea729f0166a2c5047f3f4c
JOSE CAIZA <46069965+joseru82@users.noreply.github.com>
2020-01-19 09:24:59 -0500

    Create ejemplo.sql

---
+++ sql/ejemplo.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `cloudcomputing`.`alumno` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `identificacion` VARCHAR(45) NULL,
+  `nombre` VARCHAR(45) NULL,
+  `edad` INT NULL,
+  `telefono` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
